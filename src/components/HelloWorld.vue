b8ec29b871ad672cd452c6eef089270dc88ac3d8
mac2013 <875243348@qq.com>
2018-03-31 20:56:36 +0800

    add：添加点击

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
+    <br/>
+    <router-link id="hello" to="/hello">点我</router-link>
+    <br/>
+    <input type="button" value="点我" @click="goo()"/>
   </div>
 </template>
 <script>
@@ -12,32 +16,40 @@
    *
    */
   export default {
-  name: 'HelloWorld',
-  // data:{
-  //   msg:'hell wang zhe rong yao '
-  // }s
-  data: function() {
-    return {
-      msg: 'Welcome to Your Vue.js App  mac2013 '
+    name: 'HelloWorld',
+    // data:{
+    //   msg:'hell wang zhe rong yao '
+    // }s
+    data: function () {
+      return {
+        msg: 'Welcome to Your Vue.js App  mac2013 '
+      }
+    },
+    methods:{
+      goo:function () {
+        this.$router.push("/hello")
+      }
     }
   }
-}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+  h1, h2 {
+    font-weight: normal;
+  }
+
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+
+  li {
+    display: inline-block;
+    margin: 0 10px;
+  }
+
+  a {
+    color: #42b983;
+  }
 </style>
