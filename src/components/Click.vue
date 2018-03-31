d7d7a53a44a9348162338cff7e67e54af38886b5
mac2013 <875243348@qq.com>
2018-03-31 20:36:12 +0800

    add：添加点击事件

---
+++ src/components/Click.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="mac2103" @click="show1()">
+    <h1>{{msg}}</h1>
+    <input type="button" value="按钮" @click.stop="show()"/>
+    <input/>
+  </div>
+</template>
+
+<!--
+v-on:click   ==  @click
+事件冒泡
+@click.stop
+
+默认行为
+
+键盘事件
+@keydown
+@keyup
+
+-->
+<script>
+  export default {
+    name: "hello",
+    data() {
+      return {
+        msg: "jjjj"
+      }
+    },
+    methods: {
+      show: function (ev) {
+        alert(1)
+        // ev.cancelBubble= true
+      },
+      show1: function () {
+        alert(2)
+      }
+
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
-import Hello from '@/components/Hello'
+import Hello from '@/components/Click'
 
 
 Vue.use(Router)
